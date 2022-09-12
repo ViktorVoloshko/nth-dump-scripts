8a767c7ff6a81a8885f4d560c42b07b28fd3cda9
ViktorVoloshko <viktor.voloshko@outlook.com>
2022-09-12 13:25:05 +0300

    Changed spaces to Tab

--- nth-dump-android.sh
+++ nth-dump-android.sh
@@ -4,8 +4,8 @@ echo "Enter number of servers to dump:"
 read SERVERS_QUANTITY
 for i in `seq 1 $SERVERS_QUANTITY`
 do
-    echo "Dumping $i server..."
-    ./nth-dump.android-arm -format text -noqr >> nth-dump.txt
+	echo "Dumping $i server..."
+	./nth-dump.android-arm -format text -noqr >> nth-dump.txt
 done
 echo "Done dumping. Now moving to Downloads folder."
 mv ./nth-dump.txt ./storage/downloads/nth-dump.txt
--- nth-dump-linux.sh
+++ nth-dump-linux.sh
@@ -4,7 +4,7 @@ echo "Enter number of servers:"
 read SERVERS_QUANTITY
 for i in `seq 1 $SERVERS_QUANTITY`
 do
-    echo "Dumping $i server..."
-    ./nth-dump.linux-386 -format text -noqr >> nth-dump.txt
+	echo "Dumping $i server..."
+	./nth-dump.linux-386 -format text -noqr >> nth-dump.txt
 done
 echo "Finished!"
