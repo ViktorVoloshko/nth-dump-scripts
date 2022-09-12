59f55f4b883a96a0ee7ce9095d9ed0af3ba15808
ViktorVoloshko <viktor.voloshko@outlook.com>
2022-09-12 11:56:09 +0300

    GNU/Linux range input by user

--- nth-dump-linux.sh
+++ nth-dump-linux.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-for i in {1..10}
+SERVERS_QUANTITY=0
+echo "Enter number of servers: "
+read SERVERS_QUANTITY
+for i in `seq 1 $SERVERS_QUANTITY`
 do
     echo "Dumping $i server..."
     ./nth-dump.linux-386 -format text -noqr >> nth-dump.txt
