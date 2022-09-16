22765cebbda51b6fee8e5842c7e81487fbdb9133
ViktorVoloshko <87974065+ViktorVoloshko@users.noreply.github.com>
2022-09-16 14:50:32 +0300

    Update nth-dump-android.sh

--- nth-dump-android.sh
+++ nth-dump-android.sh
@@ -5,11 +5,11 @@ read SERVERS_QUANTITY
 for i in `seq 1 $SERVERS_QUANTITY`
 do
 	echo "Dumping $i server..."
-	./nth-dump.android-arm -format text -noqr >> nth-dump.txt
+	./nth-dump.android-arm -format text -noqr >> nth-dump-tmp.txt
 done
 echo "ss:// links extraction..."
-grep -Eo "ss:.+$" ./nth-dump.txt > ./nth-dump-ss.txt
+grep -Eo "ss:.+$" ./nth-dump-tmp.txt > ./nth-dump-ss.txt
 mv ./nth-dump-ss.txt ./storage/downloads/nth-dump-ss.txt
 echo "Removing temporary files..."
-rm ./nth-dump.txt
+rm ./nth-dump-tmp.txt
 echo "Finished!"
