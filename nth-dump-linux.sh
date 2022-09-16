b2cad731f86637d07c54faeb021f1201a15d7277
ViktorVoloshko <87974065+ViktorVoloshko@users.noreply.github.com>
2022-09-16 14:49:15 +0300

    ss:// links as result

--- nth-dump-linux.sh
+++ nth-dump-linux.sh
@@ -5,6 +5,11 @@ read SERVERS_QUANTITY
 for i in `seq 1 $SERVERS_QUANTITY`
 do
 	echo "Dumping $i server..."
-	./nth-dump.linux-386 -format text -noqr >> nth-dump.txt
+	./nth-dump.linux-386 -format text -noqr >> nth-dump-tmp.txt
 done
+echo "ss:// links extraction..."
+grep -Eo "ss:.+$" ./nth-dump-tmp.txt > ./nth-dump-ss.txt
+mv ./nth-dump-ss.txt ./storage/downloads/nth-dump-ss.txt
+echo "Removing temporary files..."
+rm ./nth-dump-tmp.txt
 echo "Finished!"
